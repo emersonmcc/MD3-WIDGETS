4b08a3575e28f112a1708c3055437a528a07be5a
emersonmcc <35505704+emersonmcc@users.noreply.github.com>
2020-05-19 19:28:34 +0100

    starting calculator widget. Layout created.

---
+++ app/src/main/java/com/example/boxlightwidgets/Calculator.java
@@ -0,0 +1,94 @@
+package com.example.boxlightwidgets;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.text.InputFilter;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+public class Calculator extends Service {
+
+    private WindowManager windowManager;
+    private View calculatorView;
+    private WindowManager.LayoutParams params;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        AddCalculatorView();
+    }
+
+    public void AddCalculatorView() {
+        int layoutParamsType;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            layoutParamsType = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        }
+        else {
+            layoutParamsType = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
+        params = new WindowManager.LayoutParams(
+                500,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                layoutParamsType,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER | Gravity.CENTER;
+        params.x = 0;
+        params.y = 0;
+
+        LinearLayout interceptorLayout = new LinearLayout(this) {
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+
+                // Only fire on the ACTION_DOWN event, or you'll get two events (one for _DOWN, one for _UP)
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+
+                    // Check if the HOME button is pressed
+                    if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+
+                        Log.v("info", "BACK Button Pressed");
+
+                        // As we've taken action, we'll return true to prevent other apps from consuming the event as well
+                        return true;
+                    }
+                }
+
+                // Otherwise don't intercept the event
+                return super.dispatchKeyEvent(event);
+            }
+        };
+
+        LayoutInflater inflater = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+
+        if (inflater != null) {
+            calculatorView = inflater.inflate(R.layout.calculator_layout, interceptorLayout);
+            windowManager.addView(calculatorView, params);
+        } else {
+            Log.e("SAW-example", "Layout Inflater Service is null; can't inflate and display R.layout.floating_view");
+        }
+    }
+
+}
